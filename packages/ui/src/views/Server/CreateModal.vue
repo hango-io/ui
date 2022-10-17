3f4da1e546afd1b5f27407fbfb0283a0e8c30cca
gy <guoying06@corp.netease.com>
2022-10-17 15:16:04 +0800

    fix: 服务管理-修改，修改服务类型无法生效

--- packages/ui/src/views/Server/CreateModal.vue
+++ packages/ui/src/views/Server/CreateModal.vue
@@ -1,65 +1,19 @@
 <template>
-    <g-modal-form
-        :title="isEdit ? '修改服务' : '创建服务'"
-        :okText="isEdit ? '保存' : '创建'"
-        visible
-        :submit="handleSubmit"
-        @close="handleClose"
-    >
-        <validation-provider
-            v-slot="{ errors }"
-            name="协议类型"
-            rules="required"
-        >
-            <v-radio-group
-                label="协议类型"
-                v-model="form.ServiceType"
-                row
-                :error-messages="errors"
-            >
-            <v-radio
-                v-for="item in ProtocolType"
-                :key="item.value"
-                :label="item.text"
-                :value="item.value"
-            ></v-radio>
+    <g-modal-form :title="isEdit ? '修改服务' : '创建服务'" :okText="isEdit ? '保存' : '创建'" visible :submit="handleSubmit" @close="handleClose">
+        <validation-provider v-slot="{ errors }" name="协议类型" rules="required">
+            <v-radio-group label="服务类型" v-model="form.ServiceType" row :error-messages="errors">
+                <v-radio v-for="item in ProtocolType" :key="item.value" :disabled="isEdit" :label="item.text" :value="item.value"></v-radio>
             </v-radio-group>
         </validation-provider>
-        <validation-provider
-            v-slot="{ errors }"
-            name="服务名称"
-            rules="required"
-        >
-            <v-text-field
-                v-model="form.ServiceName"
-                label="服务名称*"
-                :error-messages="errors"
-                required
-            ></v-text-field>
+        <validation-provider v-slot="{ errors }" name="服务名称" rules="required">
+            <v-text-field v-model="form.ServiceName" label="服务名称*" :error-messages="errors" required></v-text-field>
         </validation-provider>
-        <validation-provider
-            v-slot="{ errors }"
-            name="服务标识"
-            rules="required"
-        >
-            <v-text-field
-                v-model="form.ServiceTag"
-                label="服务标识*"
-                :error-messages="errors"
-                required
-            ></v-text-field>
+        <validation-provider v-slot="{ errors }" name="服务标识" rules="required">
+            <v-text-field v-model="form.ServiceTag" label="服务标识*" :error-messages="errors" required></v-text-field>
         </validation-provider>
-        <validation-provider
-            v-slot="{ errors }"
-            name="备注信息"
-        >
-            <v-textarea
-                v-model="form.Description"
-                label="备注信息"
-                :error-messages="errors"
-            ></v-textarea>
+        <validation-provider v-slot="{ errors }" name="备注信息">
+            <v-textarea v-model="form.Description" label="备注信息" :error-messages="errors"></v-textarea>
         </validation-provider>
-
     </g-modal-form>
 </template>
 <script>
