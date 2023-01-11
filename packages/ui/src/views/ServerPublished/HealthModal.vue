cb290260c9a111fcb9675b2aa5d1d32c7369cad3
gy <guoying06@corp.netease.com>
2023-01-11 13:42:24 +0800

    fix: 文案错误

--- packages/ui/src/views/ServerPublished/HealthModal.vue
+++ packages/ui/src/views/ServerPublished/HealthModal.vue
@@ -1,61 +1,21 @@
 <template>
-<!-- TODO -->
-    <g-modal-form
-        title="健康检查"
-        visible
-        :submit="handleSubmit"
-        @close="handleClose"
-    >
-        <v-switch
-            v-model="form.ActiveSwitch"
-            label="主动检查"
-        ></v-switch>
+    <!-- TODO -->
+    <g-modal-form title="健康检查" visible :submit="handleSubmit" @close="handleClose">
+        <v-switch v-model="form.ActiveSwitch" label="主动检查"></v-switch>
 
         <template v-if="form.ActiveSwitch">
-            <validation-provider
-                v-slot="{ errors }"
-                name="检查路径"
-                rules="required"
-            >
-                <v-text-field
-                    v-model="form.Path"
-                    placeholder="请输入检查路径"
-                    label="检查路径*"
-                    :error-messages="errors"
-                    required
-                ></v-text-field>
+            <validation-provider v-slot="{ errors }" name="检查路径" rules="required">
+                <v-text-field v-model="form.Path" placeholder="请输入检查路径" label="检查路径*" :error-messages="errors" required></v-text-field>
             </validation-provider>
-            <validation-provider
-                v-slot="{ errors }"
-                name="超时时间"
-                :rules="{ required: true, between: [1, 1000000000] }"
-            >
-                <v-text-field
-                    v-model="form.Timeout"
-                    placeholder="大于 0 的整数"
-                    label="超市时间*"
-                    :error-messages="errors"
-                    required
-                    suffix="ms"
-                >
-                <template v-slot:append-outer>
-                    <g-note>是指网关转发给后端实例的请求在指定的时间内未返回，有可能是网络异常或者实例异常</g-note>
-                </template>
-            </v-text-field>
-            </validation-provider>
-            <validation-provider
-                v-slot="{ errors }"
-                name="健康状态码"
-                :rules="{ required: true, length: 3, between: [100, 999] }"
-            >
-                <v-text-field
-                    v-model="form.ExpectedStatuses"
-                    placeholder="必须为3位有效整数"
-                    label="健康状态码*"
-                    :error-messages="errors"
-                    required
-                >
+            <validation-provider v-slot="{ errors }" name="超时时间" :rules="{ required: true, between: [1, 1000000000] }">
+                <v-text-field v-model="form.Timeout" placeholder="大于 0 的整数" label="超时时间*" :error-messages="errors" required suffix="ms">
+                    <template v-slot:append-outer>
+                        <g-note>是指网关转发给后端实例的请求在指定的时间内未返回，有可能是网络异常或者实例异常</g-note>
+                    </template>
                 </v-text-field>
+            </validation-provider>
+            <validation-provider v-slot="{ errors }" name="健康状态码" :rules="{ required: true, length: 3, between: [100, 999] }">
+                <v-text-field v-model="form.ExpectedStatuses" placeholder="必须为3位有效整数" label="健康状态码*" :error-messages="errors" required></v-text-field>
             </validation-provider>
 
             <g-label>
@@ -63,15 +23,8 @@
                 <g-note>当健康实例连续N次的检查都是异常时，该实例变为异常状态，异常实例总数不超过总数的50%</g-note>
             </g-label>
             <v-row>
-                <v-col
-                    cols="12"
-                    sm="6"
-                >
-                    <validation-provider
-                        v-slot="{ errors }"
-                        name="检查间隔"
-                        :rules="{ required: true, between: [1, 1000000000] }"
-                    >
+                <v-col cols="12" sm="6">
+                    <validation-provider v-slot="{ errors }" name="检查间隔" :rules="{ required: true, between: [1, 1000000000] }">
                         <v-text-field
                             v-model="form.HealthyInterval"
                             placeholder="请输入1-1000000数字"
@@ -82,15 +35,8 @@
                         ></v-text-field>
                     </validation-provider>
                 </v-col>
-                <v-col
-                    cols="12"
-                    sm="6"
-                >
-                    <validation-provider
-                        v-slot="{ errors }"
-                        name="健康阈值"
-                        :rules="{ required: true, between: [1, 1000000000] }"
-                    >
+                <v-col cols="12" sm="6">
+                    <validation-provider v-slot="{ errors }" name="健康阈值" :rules="{ required: true, between: [1, 1000000000] }">
                         <v-text-field
                             v-model="form.HealthyThreshold"
                             placeholder="请输入1-1000000数字"
@@ -108,15 +54,8 @@
                 <g-note>当异常实例连续N次的检查都是健康时，该实例变为健康状态</g-note>
             </g-label>
             <v-row>
-                <v-col
-                    cols="12"
-                    sm="6"
-                >
-                    <validation-provider
-                        v-slot="{ errors }"
-                        name="检查间隔"
-                        :rules="{ required: true, between: [1, 1000000000] }"
-                    >
+                <v-col cols="12" sm="6">
+                    <validation-provider v-slot="{ errors }" name="检查间隔" :rules="{ required: true, between: [1, 1000000000] }">
                         <v-text-field
                             v-model="form.UnhealthyInterval"
                             placeholder="请输入1-1000000数字"
@@ -127,15 +66,8 @@
                         ></v-text-field>
                     </validation-provider>
                 </v-col>
-                <v-col
-                    cols="12"
-                    sm="6"
-                >
-                    <validation-provider
-                        v-slot="{ errors }"
-                        name="健康阈值"
-                        :rules="{ required: true, between: [1, 1000000000] }"
-                    >
+                <v-col cols="12" sm="6">
+                    <validation-provider v-slot="{ errors }" name="健康阈值" :rules="{ required: true, between: [1, 1000000000] }">
                         <v-text-field
                             v-model="form.UnhealthyThreshold"
                             placeholder="请输入1-1000000数字"
@@ -147,20 +79,12 @@
                     </validation-provider>
                 </v-col>
             </v-row>
-
         </template>
 
-        <v-switch
-            v-model="form.PassiveSwitch"
-            label="被动检查"
-        ></v-switch>
+        <v-switch v-model="form.PassiveSwitch" label="被动检查"></v-switch>
 
         <template v-if="form.PassiveSwitch">
-            <validation-provider
-                v-slot="{ errors }"
-                name="连续失败次数"
-                :rules="{ required: true, between: [1, 1000000000] }"
-            >
+            <validation-provider v-slot="{ errors }" name="连续失败次数" :rules="{ required: true, between: [1, 1000000000] }">
                 <v-text-field
                     v-model="form.ConsecutiveErrors"
                     placeholder="大于 0 的整数"
@@ -174,11 +98,7 @@
                     </template>
                 </v-text-field>
             </validation-provider>
-            <validation-provider
-                v-slot="{ errors }"
-                name="驱逐时间"
-                :rules="{ required: true, between: [1, 1000000000] }"
-            >
+            <validation-provider v-slot="{ errors }" name="驱逐时间" :rules="{ required: true, between: [1, 1000000000] }">
                 <v-text-field
                     v-model="form.BaseEjectionTime"
                     placeholder="大于 0 的整数"
@@ -192,11 +112,7 @@
                     </template>
                 </v-text-field>
             </validation-provider>
-            <validation-provider
-                v-slot="{ errors }"
-                name="最多可驱逐实例比"
-                :rules="{ required: true, between: [1, 100] }"
-            >
+            <validation-provider v-slot="{ errors }" name="最多可驱逐实例比" :rules="{ required: true, between: [1, 100] }">
                 <v-text-field
                     v-model="form.MaxEjectionPercent"
                     placeholder="大于 0 的整数"
@@ -210,11 +126,7 @@
                     </template>
                 </v-text-field>
             </validation-provider>
-            <validation-provider
-                v-slot="{ errors }"
-                name="最小健康实例比"
-                :rules="{ required: true, between: [1, 100] }"
-            >
+            <validation-provider v-slot="{ errors }" name="最小健康实例比" :rules="{ required: true, between: [1, 100] }">
                 <v-text-field
                     v-model="form.MinHealthPercent"
                     placeholder="大于 0 的整数"
@@ -228,9 +140,7 @@
                     </template>
                 </v-text-field>
             </validation-provider>
-
         </template>
-
     </g-modal-form>
 </template>
 <script>
@@ -280,13 +190,15 @@ export default {
             let chain = Promise.resolve();
 
             console.info('this.current:', this.current);
-            chain = chain.then(() => this.axios({
-                action: 'DescribeHealthCheckRule',
-                params: {
-                    VirtualGwId: this.GwId,
-                    ServiceId: this.ServiceId,
-                },
-            }));
+            chain = chain.then(() =>
+                this.axios({
+                    action: 'DescribeHealthCheckRule',
+                    params: {
+                        VirtualGwId: this.GwId,
+                        ServiceId: this.ServiceId,
+                    },
+                })
+            );
 
             chain = chain.then(({ HealthCheckRule }) => {
                 this.form = HealthCheckRule || this.form;
